9299fca07703498e3c0bbfdea89a0a3028b3b614
MRFolio <roose.markus@gmail.com>
2021-05-03 23:05:53 +0300

    add timer

--- src/components/Chessboard.js
+++ src/components/Chessboard.js
@@ -1,11 +1,20 @@
 import { useState } from 'react';
 import styles from './Chessboard.module.scss';
+import Timer from './Timer';
 
 const Chessboard = () => {
   const [time, setTime] = useState(0);
+  const [userInfoShown, setUserInfoShown] = useState(false);
+  const [currentActiveMove, setCurrentActiveMove] = useState(0);
+  const [paused, setPaused] = useState(false);
 
   const handleRematchClick = () => {
+    setPaused(!paused);
     // timer start
+  };
+
+  const handleMouseEnter = () => {
+    setUserInfoShown(true);
   };
 
   return (
@@ -85,20 +94,39 @@ const Chessboard = () => {
       </div>
       <div className={styles.dashboard}>
         <div></div>
-        <div className={styles.clockTop}>{time}</div>
+        <div className={styles.clockTop}>
+          <Timer paused={paused} />
+        </div>
         <div className={styles.user}>
+          <div className={styles.circle}></div>
+          <p onMouseEnter={handleMouseEnter} className={styles.name}>
+            alihanturgut
+          </p>
+          <p className={styles.rating}>1378</p>
+          {userInfoShown && <article></article>}
+        </div>
+        <div className={styles.buttons}>
+          <button type="button">Flip board</button>
+          <button disabled type="button">
+            Flip board
+          </button>
+          <button type="button">Flip board</button>
+          <button type="button">Flip board</button>
+          <button type="button">Flip board</button>
+        </div>
+        <div className={styles.controls}>
+          {' '}
+          <button onClick={handleRematchClick} className={styles.button}>
+            Rematch
+          </button>
+          <button className={styles.rematch}>New Opponent</button>
+        </div>
+        <div className={styles.user}>
+          {' '}
           <div className={styles.circle}></div>
           <p className={styles.name}>alihanturgut</p>
           <p className={styles.rating}>1378</p>
         </div>
-        <div className={styles.controls}>
-          {' '}
-          <div onClick={handleRematchClick} className={styles.button}>
-            Rematch
-          </div>
-          <div className={styles.rematch}>New Opponent</div>
-        </div>
-        <div className={styles.user}>tresta2323</div>
         <div className={styles.clockBottom}>03:00</div>
       </div>
     </section>
---
+++ src/components/Timer.js
@@ -0,0 +1,38 @@
+import { useEffect, useState } from 'react';
+
+const Timer = ({ paused }) => {
+  const [minutes, setMinutes] = useState(3);
+  const [seconds, setSeconds] = useState(0);
+
+  useEffect(() => {
+    let myInterval = setInterval(() => {
+      if (seconds > 0) {
+        setSeconds(seconds - 1);
+      }
+      if (seconds === 0) {
+        if (minutes === 0 || paused) {
+          clearInterval(myInterval);
+        } else {
+          setMinutes(minutes - 1);
+          setSeconds(59);
+        }
+      }
+    }, 1000);
+    return () => {
+      clearInterval(myInterval);
+    };
+  });
+
+  return (
+    <div>
+      {minutes === 0 && seconds === 0 ? null : (
+        <h1>
+          {' '}
+          {minutes}:{seconds < 10 ? `0${seconds}` : seconds}
+        </h1>
+      )}
+    </div>
+  );
+};
+
+export default Timer;
